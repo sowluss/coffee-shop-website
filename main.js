98b5dbfccab81a575f454f752c5f2471f7bf46b5
sowluss <sowluss@gmail.com>
2018-11-17 00:28:26 +0200

    worked on JS form validation, .js .css .html adjusted accordingly

--- main.js
+++ main.js
@@ -3,21 +3,40 @@ evenListeners();
 function evenListeners() {
   const ui = new UI()
 
-  // Hide preloader after all the content finished loading
+  // Preloader 
   window.addEventListener('load', function () {
     ui.hidePreloader();
   })
 
-  // Navigation toggle button
+  // Toggle button
   document.querySelector('.toggle-btn').addEventListener('click', function () {
     ui.showNav();
   })
 
-  // video switch on/off button
+  // Video control
   document.querySelector('.video-switch').addEventListener('click', function () {
     ui.videoControls();
   })
+
+  // Submit form
+  document.querySelector('.drink-form').addEventListener('submit', function(e){
+    e.preventDefault();
+    const name = document.querySelector('.input-name').value;
+    const surname = document.querySelector('.input-lastname').value;
+    const email = document.querySelector('.input-email').value;
+
+    let value = ui.checkEmpty(name, surname, email);
+    
+    if(value) {
+      ui.showFeedback('Your entry submitted successfully', 'success');
+    }
+    else {
+      ui.showFeedback('Please, complete all fields', 'error')
+    }
+  })
 }
+
+
 
 // Construction function
 function UI() {}
@@ -42,3 +61,39 @@ UI.prototype.videoControls = function () {
     document.querySelector('.video-item').play();
   }
 }
+
+// check for empty form inputs
+
+UI.prototype.checkEmpty = function(name, surname, email) {
+  let result;
+  if(name === '' || surname === '' || email === '') {
+
+    result = false;
+  }
+  else {
+    result = true;
+  }
+  return result;
+}
+
+UI.prototype.showFeedback = function(text, type) {
+  if(type === 'success') {
+    let feedback = document.querySelector('.drinkForm-feedback');
+    feedback.classList.add('success');
+    feedback.innerText = text;
+    this.removeAlert('success');
+  }
+  else if(type === 'error') {
+    let feedback = document.querySelector('.drinkForm-feedback');
+    feedback.classList.add('error');
+    feedback.innerText = text;
+    this.removeAlert('error');
+  }
+}
+// remove alert
+UI.prototype.removeAlert = function(type) {
+  setTimeout(function(){
+    document.querySelector('.drinkForm-feedback').classList.remove(type);
+  }, 3000);
+}
+
